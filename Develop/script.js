1183f449853faf0e279d7658199f83766ea603b1
Dominika <domi_nika@hotmail.co.uk>
2021-03-27 13:35:39 +0000

    app console logs current hour

--- Develop/script.js
+++ Develop/script.js
@@ -1,2 +1,5 @@
 var today = moment();
-$("#currentDay").text(today.format("MMM Do, YYYY"));
+$("#currentDay").text(today.format("Do [of] MMM YYYY"));
+
+var currentTime = moment().format('HH');
+console.log(currentTime)
