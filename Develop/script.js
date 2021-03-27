06cc3a6b04cb8bd98295eb65ef85cf6ceab71f2d
Dominika <domi_nika@hotmail.co.uk>
2021-03-27 13:55:49 +0000

    time is refreshed every minute

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,27 @@
 var today = moment();
-$("#currentDay").text(today.format("Do [of] MMM YYYY"));
 
-var currentTime = moment().format('HH');
-console.log(currentTime)
+function populatePageInformation () {
+  populateDayInformation ();
+  startTimer ();
+  currentTime = identifyCurrentTime ();
+  colourCodeTimeBlocks ();
+};
+
+function populateDayInformation () {
+  $("#currentDay").text(today.format("Do [of] MMM YYYY"));
+};
+
+function startTimer () {
+  setInterval(identifyCurrentTime, 60000);
+;}
+
+function identifyCurrentTime () {
+  //every x time update current time
+  console.log("time")
+};
+
+function colourCodeTimeBlocks () {
+  console.log("hello")
+};
+
+$("document").ready(populatePageInformation);
