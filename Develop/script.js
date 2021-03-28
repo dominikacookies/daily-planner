9da2d80f7d69d85ee411c7a0fc2c3d3a98848d93
Dominika <domi_nika@hotmail.co.uk>
2021-03-28 12:27:32 +0100

    current time is updated every minute

--- Develop/script.js
+++ Develop/script.js
@@ -1,21 +1,21 @@
 let today = moment();
 let currentTime = identifyCurrentTime();
-timeBlockElementsArray = ["hello","hello","hello"];
 
 function identifyCurrentTime () {
   currentHour = today.format("HH");
+  console.log(currentHour);
 };
 
 //every minute trigger check what the current time is
 function updateCurrentTimeTimer () {
   setInterval(identifyCurrentTime, 60000);
-;}
+};
 
 //colour code time blocks for past/present/future events
 function colourCodeTimeBlocks () {
   console.log("hello")
   // turn html elements into an array
-  const blockTime = $("#blockTime").attr("data-time");
+  //const blockTime = $("#blockTime").attr("data-time");
   // if time block attr is less than current time add class .past
 
   // else if time block attr is equal to current time then add class current
@@ -26,8 +26,8 @@ function colourCodeTimeBlocks () {
 function populatePageInformation () {
   // Populate day information in the header
   $("#currentDay").text(today.format("Do [of] MMM YYYY"));
-  //updateCurrentTimeTimer();
-  //timeBlockElementsArray.forEach(colourCodeTimeBlocks())
+  updateCurrentTimeTimer();
+  //$('.row').each(colourCodeTimeBlocks());
 };
 
 $("document").ready(populatePageInformation);
