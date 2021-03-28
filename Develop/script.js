6ce045fedf87a4afb35eb7ad2bc4ddb0f51e6ed8
Dominika <domi_nika@hotmail.co.uk>
2021-03-28 13:36:04 +0100

    data time attr for each row is recognised

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,5 @@
-let today = moment();
-let currentTime = identifyCurrentTime();
+today = moment();
+currentTime = identifyCurrentTime();
 
 function identifyCurrentTime () {
   currentHour = today.format("HH");
@@ -12,23 +12,32 @@ function updateCurrentTimeTimer () {
 };
 
 //colour code time blocks for past/present/future events
-function colourCodeTimeBlocks () {
-  console.log("hi")
-  // turn html elements into an array
-  //const blockTime = $("#blockTime").attr("data-time");
+//unction colourCodeTimeBlocks () {
+  const blockTime = ($(this).data("time"));
+  
+  //$(".hour").attr("data-time");
+  //console.log(blockTime);
+  //if (blockTime) {
+
+  //} else if () {
+
+  //} else {
+
+  //}
   // if time block attr is less than current time add class .past
 
   // else if time block attr is equal to current time then add class current
 
   // else add class future
-};
+//};
 
 function populatePageInformation () {
   // Populate day information in the header
   $("#currentDay").text(today.format("Do [of] MMM YYYY"));
   updateCurrentTimeTimer();
   $(".row").each(function() {
-    colourCodeTimeBlocks ()
+    let blockTime = $(this).children(".hour").attr("data-time");
+    console.log(blockTime);
   });
 };
 
