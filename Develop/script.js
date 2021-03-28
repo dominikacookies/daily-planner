58f9b293ec3374e11a25591067ef2de319506acf
Dominika <domi_nika@hotmail.co.uk>
2021-03-28 14:10:23 +0100

    time block class is added based on current time, bug with 9 am slot

--- Develop/script.js
+++ Develop/script.js
@@ -3,7 +3,7 @@ currentTime = identifyCurrentTime();
 
 function identifyCurrentTime () {
   currentHour = today.format("HH");
-  console.log(currentHour);
+  return currentHour;
 };
 
 //every minute trigger check what the current time is
@@ -12,33 +12,27 @@ function updateCurrentTimeTimer () {
 };
 
 //colour code time blocks for past/present/future events
-//unction colourCodeTimeBlocks () {
-  const blockTime = ($(this).data("time"));
+function colourCodeTimeBlocks () {
+  $(".row").each(function() {
+    let blockTime = $(this).children(".hour").attr("data-time");
+    console.log(blockTime);
+    console.log(currentTime);
   
-  //$(".hour").attr("data-time");
-  //console.log(blockTime);
-  //if (blockTime) {
-
-  //} else if () {
-
-  //} else {
-
-  //}
-  // if time block attr is less than current time add class .past
-
-  // else if time block attr is equal to current time then add class current
-
-  // else add class future
-//};
+    if (blockTime == currentTime) {
+      $(this).children("textarea").addClass("present");
+    } else if (blockTime < currentTime) {
+      $(this).children("textarea").addClass("past");
+    } else {
+      $(this).children("textarea").addClass("future");
+    };
+  });
+};
 
 function populatePageInformation () {
   // Populate day information in the header
   $("#currentDay").text(today.format("Do [of] MMM YYYY"));
   updateCurrentTimeTimer();
-  $(".row").each(function() {
-    let blockTime = $(this).children(".hour").attr("data-time");
-    console.log(blockTime);
-  });
+  colourCodeTimeBlocks();
 };
 
 $("document").ready(populatePageInformation);
